ae83899e5b30f26f32e35dd56ca63c5ad3d9e746
ryglearn <raffyoga99@gmail.com>
2026-01-15 18:33:05 +0700

    refactor: menambahkan komentar

--- src/utils/AutoGenerateCode.js
+++ src/utils/AutoGenerateCode.js
@@ -1,9 +1,10 @@
 import pool from "../config/db.js";
 
 export const autoGenerateCode = (lastCode, prefix, digits) => {
-  if (!lastCode) return `${prefix}-${"1".padStart(digits, "0")}`;
+  if (!lastCode) return `${prefix}-${"1".padStart(digits, "0")}`; // jika lascode tidak ada 
 
   const numberCode = parseInt(lastCode.slice(prefix.length + 1), 10);
   const nextNumberCode = numberCode + 1;
-  return `${prefix}-${String(nextNumberCode).padStart(digits, "0")}`;
+  return `${prefix}-${String(nextNumberCode).padStart(digits, "0")}`; // digits -> jumlah angka, "0" mau diisi apa selain code nya 
 };
+
